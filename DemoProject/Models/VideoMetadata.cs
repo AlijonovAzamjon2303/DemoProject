251ba332dee3ed7697d54fce87ed57555728a7ce
A'zamjon Alijonov <alijonovrobo@gmail.com>
2024-05-15 13:49:17 +0500

    added-models

---
+++ DemoProject/Models/VideoMetadata.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoProject.Models
+{
+    public class VideoMetadata
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
